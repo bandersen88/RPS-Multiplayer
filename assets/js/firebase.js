bb4bd3b93843c48b4be3948c23329d60596d65ce
Production - Ben Andersen <bandersen@delkorsystems.com>
2019-03-13 09:30:36 -0500

    not working trying new stuff

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -15,12 +15,58 @@
     //initialize that database and the clickCounter variable
     //TODO initialize all variables
     var database = firebase.database();
-    var clickCounter = 10;
+    //clears past data
+    // database.ref().remove();
+  
+    var move = "";
+    var text = "newText";
 
-    database.ref().set({
-        clickCount: clickCounter
+    // database.ref("texts").push({
+    //     text: text
+    // });
+
+    // database.ref().orderByChild("dateAdded").on("child_added", function(childSnapshot) { });
+    
+    database.ref("texts").limitToLast(5).on("child_added", function(childSnapshot) {
+        alert("hereiam");
+        alert(childSnapshot.val().text);
+        // alert("now here");
     });
 
+    $(".move").on("click", function(event) {
+        event.preventDefault();
+
+        move = $(this).attr("id");
+
+        database.ref().push({
+            move: move
+        });
+
+    })
+
+    $("#chat").on("submit", function(event) {
+        event.preventDefault();
+
+        text = $("#message").val();
+        database.ref("texts").push({
+            text: text
+        });
+    })
+
+    // database.ref("texts").orderByChild("dateAdded").limitToLast(1).on("child_added", function(childSnapshot) {
+    //     // alert("here it is"+snapshot.child(0).val().text);
+    //     alert(childSnapshot.val());
+    //     // snapshot.forEach(function(childSnapshot) {
+    //     //     alert(childSnapshot.val());
+    //     // })
+    //     alert("sup");
+    //     $("p:first-of-type").remove();
+    //     var $p = $("<p>");
+    //     alert(childSnapshot.val());
+    //     $("p:last-of-type").insertAfter($p);
+
+
+    // });
     //***TODO need to know how to connect variables to connections */
 
     //TODO game starts if and only if 2 concurrent connections exist to the db
@@ -54,4 +100,12 @@
     // }, function(errorObject) {
     //   console.log("The read failed: " + errorObject.code);
     // });
+
+
+    //push for multiple records
+    //set to change existing records
+
+    //I kind of can't believe it.  But I am actually working on it, whatever needs to happen here in order for me to get a job
+    //as a software engineer.  Going to Dunwoody was dumb.  That was being stuck in the past.  Should have jumped aboard the
+    //software train.
 
